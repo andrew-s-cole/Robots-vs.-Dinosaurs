4c27aabd6e0acd89b902d8d867b432ae319f0af8
andrew-s-cole <andrew.s.cole@gmail.com>
2022-04-04 11:14:54 -0400

    Initial commit

---
+++ robot.py
@@ -0,0 +1,11 @@
+from weapon import Weapon
+
+
+class Robot:
+    def __init__(self, name):
+        self.name = ''
+        self.health = 300
+        self.active_weapon = Weapon
+
+    def attack(self, dinosaur):
+        pass
