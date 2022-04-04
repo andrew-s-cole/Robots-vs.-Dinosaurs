111a944b42ba35af23611a36fdc4c85b8b367167
andrew-s-cole <andrew.s.cole@gmail.com>
2022-04-04 11:21:57 -0400

    Created dinosaur class

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = ''
+        self.attack_power = 10, 30
+        self.health = 300
+
+    def attack(self, robot):
+        pass
